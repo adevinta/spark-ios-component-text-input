738768244518cfd2a57e908a5145a45f2fdc0e0b
Robin Lemaire <robin.lemaire@adevinta.com>
2024-10-16 17:27:06 +0200

    [TextInput-1087] Add UIKit version

---
+++ Sources/Core/TextEditor/ViewModel/TextEditorViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  TextEditorViewModel.swift
+//  SparkEditor
+//
+//  Created by robin.lemaire on 05/09/2024.
+//  Copyright © 2024 Adevinta. All rights reserved.
+//
+
+import Foundation
+import SparkTheming
+
+// sourcery: AutoPublisherTest, AutoViewModelStub
+final class TextEditorViewModel: TextInputViewModel {
+
+    // MARK: - Published Properties
+
+    @Published private(set) var isPlaceholder: Bool = false
+    @Published private(set) var shouldUpdateVerticalSpacing: Int = 0
+
+    // MARK: - Private Properties
+
+    private let getVerticalSpacingUseCase: TextEditorGetVerticalSpacingUseCaseable
+
+    // MARK: - Initialization
+
+    init(
+        theme: Theme,
+        intent: TextEditorIntent,
+        getVerticalSpacingUseCase: TextEditorGetVerticalSpacingUseCaseable = TextEditorGetVerticalSpacingUseCase()
+    ) {
+        self.getVerticalSpacingUseCase = getVerticalSpacingUseCase
+
+        super.init(theme: theme, intent: intent, borderStyle: .roundedRect)
+    }
+
+    // MARK: - Update
+
+    func contentChanged(with text: String) {
+        self.isPlaceholder = text.isEmpty
+    }
+
+    // MARK: - Getter
+
+    func getVerticalSpacing(from height: CGFloat) -> CGFloat {
+        self.getVerticalSpacingUseCase.execute(
+            height: height,
+            font: self.font
+        )
+    }
+
+    // MARK: - Setter
+
+    override func setFont() {
+        super.setFont()
+
+        self.shouldUpdateVerticalSpacing += 1
+    }
+}
